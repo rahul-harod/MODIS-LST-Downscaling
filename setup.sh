3415d4167d6ff937c2718f4bd647013718b6d782
Rahul Harod <56213859+rahul-harod@users.noreply.github.com>
2024-05-15 00:04:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,1 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"rharod4@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+
