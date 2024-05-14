498c5a2a3ef38c41445ae90aca0752d3cf0d49b5
Rahul Harod <56213859+rahul-harod@users.noreply.github.com>
2024-05-14 23:39:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,7 +142,7 @@ def Predictions(modisWithClosestLandsat):
     min_ = np.nanpercentile(df1['ANN_LST'], 1)
     max_ = np.nanpercentile(df1['ANN_LST'], 99)
     
-    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(15, 3.5), gridspec_kw={"width_ratios": [1, 1, 0.05]})
+    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(7, 3.5), gridspec_kw={"width_ratios": [1, 1, 0.05]})
     fig.subplots_adjust(wspace=0.1)
     im1 = data['LST_Day_1km'].plot(ax=ax1, cmap='jet', vmin=min_, vmax=max_)
     im2 = data['ANN_LST'].plot(ax=ax2, cmap='jet', vmin=min_, vmax=max_)
