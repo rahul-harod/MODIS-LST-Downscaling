05d53d3e4c2a0e520e633b0ec85fd0a4f69bc43f
Rahul Harod <56213859+rahul-harod@users.noreply.github.com>
2024-05-17 15:11:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ from ee import oauth
 import resnet
 
 def add_logo():
-    st.sidebar.image("iitb_logo.png", width=150)
+    st.sidebar.image("iitb_logo.png", width=200)
     st.markdown(
         """
         &lt;style&gt;
