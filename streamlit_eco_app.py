05f9a4476924faebb5b0cb80ddb719049d5f3554
Rahul Harod <56213859+rahul-harod@users.noreply.github.com>
2024-06-09 03:17:42 +0530

    Update streamlit_eco_app.py

--- streamlit_eco_app.py
+++ streamlit_eco_app.py
@@ -173,9 +173,7 @@ def Predictions_ANN(modisWithClosestLandsat,date_str,selected_lst_type,selected_
     data['ANN_LST'] = merged_df['ANN_LST']
     data['ANN_LST'].attrs = {'long_name': 'ANN LST (K)', 'AREA_OR_POINT': 'Area', 'grid_mapping': 'spatial_ref'}
     data['Original_MODIS_LST'].attrs = {'long_name': 'MODIS LST (K)', 'AREA_OR_POINT': 'Area', 'grid_mapping': 'spatial_ref'}
-    ee_image = geemap.numpy_to_ee(data['ANN_LST'].to_array().squeeze().transpose('y', 'x'))
-
-    map.addLayer(ee_image, {}, 'Xarray Image')
+    map.add_raster(data['ANN_LST'],{},'R')
 
 
     # Plot multiple images in subplots
